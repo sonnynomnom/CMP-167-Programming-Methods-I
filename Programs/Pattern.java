52d75ad52e44e94c138261ff283f8633c05d9cd3
Sonny <sonnynomnom@gmail.com>
2017-08-28 19:46:18 -0400

    Week 1

--- Programs/Pattern.java
+++ Programs/Pattern.java
@@ -5,10 +5,10 @@ public class Pattern {
 
     public static void main(String[] args) {
 
-        System.out.println("      0");
-        System.out.println("    1 2");
-        System.out.println("  3 4 5");
-        System.out.println("6 7 8 9");
+        System.out.println("       1");
+        System.out.println("     2 3");
+        System.out.println("   4 5 6");
+        System.out.println("7 8 9 10");
 
     }
 }
