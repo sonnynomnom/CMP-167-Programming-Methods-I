dc49fd3d2ab1a6c2ea73b8695822307e4466a544
Sonny <sonnynomnom@gmail.com>
2017-09-18 23:34:26 -0400

    Week 1

---
+++ Programs/HelloWorld.java
@@ -0,0 +1,12 @@
+// Sonny Li
+// CMP 167: Programming Methods I
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ Programs/Pattern.java
@@ -0,0 +1,15 @@
+// Sonny Li
+// CMP 167: Programming Methods I
+
+public class Pattern {
+
+    public static void main(String[] args) {
+
+        System.out.println("       1");
+        System.out.println("     2 3");
+        System.out.println("   4 5 6");
+        System.out.println("7 8 9 10");
+
+    }
+
+}
