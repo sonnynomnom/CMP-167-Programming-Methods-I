b0161419cb37d64c5408598fe4a96995242f1dda
Sonny <sonnynomnom@gmail.com>
2017-09-18 23:35:09 -0400

    Week 3

---
+++ Programs/FlipCoin.java
@@ -0,0 +1,19 @@
+// Sonny Li
+// CMP 167: Programming Methods I
+
+public class FlipCoin {
+
+    public static void main(String[] args) {
+
+        if (Math.random() < 0.5)
+        {
+            System.out.println("Heads");
+        }
+        else
+        {
+            System.out.println("Tails");
+        }
+
+    }
+    
+}
